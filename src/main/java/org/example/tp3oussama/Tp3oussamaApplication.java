5e64029a14b50c3c0e480d73212e36e09eaf2bcb
unknown <oussama@gmail.com>
2024-04-02 11:53:58 +0000

    first commit

---
+++ src/main/java/org/example/tp3oussama/Tp3oussamaApplication.java
@@ -0,0 +1,15 @@
+package org.example.tp3oussama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+
+Tp3oussamaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp3oussamaApplication.class, args);
+    }
+
+}
